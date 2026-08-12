42f1acfcc0f819b37c541b3c92d54860076f47f5
Maraysa <maraysavaanessamoreira@gmail.com>
2026-08-12 10:55:50 -0300

    Simplica dashboard e personaliza sidebar com marca ImobFlow

--- frontend/src/views/admin/default/index.tsx
+++ frontend/src/views/admin/default/index.tsx
@@ -1,18 +1,11 @@
-import MiniCalendar from "components/calendar/MiniCalendar";
 import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import PieChartCard from "views/admin/default/components/PieChartCard";
-import { IoMdHome } from "react-icons/io";
-import { IoDocuments } from "react-icons/io5";
+
 import { MdBarChart, MdDashboard } from "react-icons/md";
 
 import Widget from "components/widget/Widget";
 import CheckTable from "views/admin/default/components/CheckTable";
-import ComplexTable from "views/admin/default/components/ComplexTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import TaskCard from "views/admin/default/components/TaskCard";
+
 import tableDataCheck from "./variables/tableDataCheck";
-import tableDataComplex from "./variables/tableDataComplex";
 
 const Dashboard = () => {
   return (
@@ -22,70 +15,46 @@ const Dashboard = () => {
       <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
         <Widget
           icon={<MdBarChart className="h-7 w-7" />}
-          title={"Earnings"}
-          subtitle={"$340.5"}
-        />
-        <Widget
-          icon={<IoDocuments className="h-6 w-6" />}
-          title={"Spend this month"}
-          subtitle={"$642.39"}
-        />
-        <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={"Sales"}
-          subtitle={"$574.34"}
+          title={"Imóveis Cadastrados"}
+          subtitle={"0"}
         />
         <Widget
           icon={<MdDashboard className="h-6 w-6" />}
-          title={"Your Balance"}
-          subtitle={"$1,000"}
+          title={"Contratos Ativos"}
+          subtitle={"0"}
         />
         <Widget
           icon={<MdBarChart className="h-7 w-7" />}
-          title={"New Tasks"}
-          subtitle={"145"}
+          title={"Proprietários"}
+          subtitle={"0"}
         />
         <Widget
-          icon={<IoMdHome className="h-6 w-6" />}
-          title={"Total Projects"}
-          subtitle={"$2433"}
+          icon={<MdDashboard className="h-6 w-6" />}
+          title={"Inquilinos"}
+          subtitle={"0"}
+        />
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={"Manutenções Pendentes"}
+          subtitle={"0"}
+        />
+        <Widget
+          icon={<MdDashboard className="h-6 w-6" />}
+          title={"Receita do Mês"}
+          subtitle={"R$ 0,00"}
         />
       </div>
 
       {/* Charts */}
 
       <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2">
-        <TotalSpent />
         <WeeklyRevenue />
       </div>
 
-      {/* Tables & Charts */}
+      {/* Tables */}
 
-      <div className="mt-5 grid grid-cols-1 gap-5 xl:grid-cols-2">
-        {/* Check Table */}
-        <div>
-          <CheckTable tableData={tableDataCheck} />
-        </div>
-
-        {/* Traffic chart & Pie Chart */}
-
-        <div className="grid grid-cols-1 gap-5 rounded-[20px] md:grid-cols-2">
-          <DailyTraffic />
-          <PieChartCard />
-        </div>
-
-        {/* Complex Table , Task & Calendar */}
-
-        <ComplexTable tableData={tableDataComplex} />
-
-        {/* Task chart & Calendar */}
-
-        <div className="grid grid-cols-1 gap-5 rounded-[20px] md:grid-cols-2">
-          <TaskCard />
-          <div className="grid grid-cols-1 rounded-[20px]">
-            <MiniCalendar />
-          </div>
-        </div>
+      <div className="mt-5 grid grid-cols-1 gap-5">
+        <CheckTable tableData={tableDataCheck} />
       </div>
     </div>
   );
